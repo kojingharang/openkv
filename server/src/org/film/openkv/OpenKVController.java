149876e8e91c1f75d393a1eba3b3460aef2fb985
i2kantak <i2kantak@dac48724-fc82-ddfb-b5c2-0a06a151bfd4>
2010-08-03 18:13:50 +0000

    - support Text Properties and int Properties

--- server/src/org/film/openkv/OpenKVController.java
+++ server/src/org/film/openkv/OpenKVController.java
@@ -1,9 +1,6 @@
 package org.film.openkv;
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.servlet.http.*;
 
 public class OpenKVController {
@@ -100,55 +97,6 @@ public class OpenKVController {
 		
 	}
 
-	public ResponseData putData(String serviceName, String okvKey, String propValue, String reqId, String callback) {
-
-		UserDataManager udm = UserDataManager.getInstance();
-		// put Services Table 
-		UserData serviceData = new UserData("Services", serviceName);
-		Map<String,Object> serviceProp = new HashMap<String, Object>();
-		serviceProp.put("AccessDate", new Date());
-		serviceProp.put("ServiceName", serviceName);
-		serviceData.setProperties(serviceProp);
-		try {
-			udm.set(serviceData);
-		}
-		catch(Exception e) {
-			ResponseData resData = new ResponseData(ResponseData.NGCODE, e.getMessage(), reqId, callback);
-			return resData;
-		}
-		
-		UserData userData = new UserData(serviceName, okvKey);
-
-		
-		userData.setPropertiesFromJSON(propValue);
-		
-
-		try {
-			udm.set(userData);
-			ResponseData resData = new ResponseData(userData, reqId, callback);
-			return resData;
-		}
-		catch(Exception e) {
-			ResponseData resData = new ResponseData(ResponseData.NGCODE, e.getMessage(), reqId, callback);
-			return resData;
-		}
-		
-	}
-		
-	public ResponseData getData(String serviceName, String okvKey, String reqId, String callback, String offset, String limit) {
-		
-		try {
-			UserDataManager udm = UserDataManager.getInstance();
-			List<UserData> userDataList = udm.get(serviceName, okvKey, offset, limit);
-			ResponseData resData = new ResponseData(userDataList, reqId, callback);
-			
-			return resData;
-		}
-		catch(Exception e) {
-			ResponseData resData = new ResponseData(ResponseData.NGCODE, e.getMessage(), reqId, callback);
-			return resData;
-		}
-	}
 	
 	private ResponseData returnErrorResponse (HttpServletRequest req, String message) {
 		ResponseData resData = new ResponseData(ResponseData.NGCODE, message, req.getParameter("rid"), req.getParameter("callback"));
