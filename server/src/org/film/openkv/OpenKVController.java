6d07504bfd89f7d8908619b0f8db08710189b7b7
i2kantak <i2kantak@dac48724-fc82-ddfb-b5c2-0a06a151bfd4>
2010-08-03 07:40:50 +0000

    - modified bug related putting Services entity

--- server/src/org/film/openkv/OpenKVController.java
+++ server/src/org/film/openkv/OpenKVController.java
@@ -35,14 +35,9 @@ public class OpenKVController {
 		UserDataManager udm = UserDataManager.getInstance();
 		
 		// put Services Table
-		UserData serviceData = new UserData("Services", serviceName);
-		Map<String, Object> serviceProp = new HashMap<String, Object>();
-		serviceProp.put("AccessDate", new Date());
-		serviceProp.put("ServiceName", serviceName);
-		serviceData.setProperties(serviceProp);
-		
+
 		try {
-			udm.set(serviceData);
+			udm.setService(serviceName);
 		}
 		catch(Exception e) {
 			resData = returnErrorResponse(req, e.getMessage());
